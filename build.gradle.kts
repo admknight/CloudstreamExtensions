8e39f865e737b1082688ed021cd8750bf99fcf34
admknight <badass.insane@gmail.com>
2026-05-28 08:21:35 +0500

    Fix: Add missing JSON dependencies and update build workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,6 +80,8 @@ subprojects {
         implementation(kotlin("stdlib")) // Adds Standard Kotlin Features
         implementation("com.github.Blatzar:NiceHttp:0.4.11") // HTTP Lib
         implementation("org.jsoup:jsoup:1.18.3") // HTML Parser
+        implementation("com.google.code.gson:gson:2.10.1") // JSON Parser
+        implementation("org.json:json:20240303") // JSON Parser
         // IMPORTANT: Do not bump Jackson above 2.13.1, as newer versions will
         // break compatibility on older Android devices.
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1") // JSON Parser
